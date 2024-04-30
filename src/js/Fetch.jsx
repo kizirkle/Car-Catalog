2afd5cc2c789cd918a8772471b9e56c4becbc71f
Kork <hail3yp@gmail.com>
2024-04-30 15:57:50 -0400

    fixed bug that prevented car's make and model from showing up

--- src/js/Fetch.jsx
+++ src/js/Fetch.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import Car from './components/Car'
+import Filter from './components/Filter'
 
 const Fetch = () => {
   const [cars, setCars] = useState([]);
+  const [makes, setMakes] = useState([]);
 
   useEffect(() => {
     fetch('https://exam.razoyo.com/api/cars')
@@ -12,14 +14,15 @@ const Fetch = () => {
       .then((data) => {
         console.log(data);
         setCars(data.cars);
+        setMakes(data.makes);
       });
   }, []);
 
   return (
     <div>
-      
+        <Filter />
       {cars.map((car) => (
-        <Car key={car.id}/>
+        <Car key={car.id} year={car.year} make={car.make} model={car.model} />
       ))}
     </div>
   );
--- src/js/components/Filter.jsx
+++ src/js/components/Filter.jsx
@@ -1,9 +1,19 @@
-export default function Filter() {
+import {useState} from 'react'
+export default function Filter(make) {
 //API call to filter Cars
+const [input, setInput] = useState('')
+
+const fetchedData = make
+
     return(
         <div className="m-4 justify-content-end col row">
                 <p className="col-1 align-self-center">Filter By Make:</p>
-                <input className="col-2"></input>
+                <input 
+                className="col-2" 
+                placeholder="Any" 
+                value={input} 
+                onChange={(e) => setInput(e.target.value)} 
+                />
         </div>
     )
 }
