df193428488432ced2c8d5d648600dfdfa38391e
Kork <hail3yp@gmail.com>
2024-05-01 14:13:13 -0400

    fixing vite.config.js to work in development

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/cars': {
-        target: 'https://exam.razoyo.com/api',
+      '/api': {
+        target: 'https://exam.razoyo.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        
       },
     },
   },
